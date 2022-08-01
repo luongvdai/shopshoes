9495a6b00a7900f12a2511e23493a435a1cfbbf8
Luong Van Dai <80164034+luongvdai@users.noreply.github.com>
2022-08-01 22:04:48 +0700

    up
    up

---
+++ Project_Shopshoes/Project_PRN211_TEAM7/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Project_PRN211_TEAM7.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Project_PRN211_TEAM7.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        PROJECT_PRN211_SHOES_APPContext db = new PROJECT_PRN211_SHOES_APPContext();
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = HttpContext.Session.GetString("username");
+            ViewBag.Brand = db.Brands.ToList();
+            return View(db.Products.ToList()) ;
+        }
+
+        
+
+       
+        
+
+    }
+}
